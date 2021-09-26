5d87549dddd55a44d37cf3bc04dd9c019374c6d7
Nagashree01 <nagahegde2001@gmail.com>
2021-09-26 12:31:19 +0530

    updated styles.css

--- app.js
+++ app.js
@@ -1,4 +1,35 @@
-// console.log("to check if script is working");
-// var userName = prompt("Give username")
-// alert("script is working " +userName);
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+//var serverURL ="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text){
+    return serverURL + "?" +"text=" +text
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time")
+}
+
+
+function clickHandler() {
+    var inputText = txtInput.value;
+
+    fetch(getTranslationURL(inputText))
+            .then(response => response.json())
+            .then(json=> {
+                var translatedText = json.contents.translated;
+                outputDiv.innerText = translatedText;
+                })
+            .catch(errorHandler)
+
+};
+
+btnTranslate.addEventListener("click", clickHandler)
+
+
 
